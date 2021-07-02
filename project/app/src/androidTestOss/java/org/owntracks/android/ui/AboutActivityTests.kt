7f7baa15b7f3c9793a0b6ad1ee90ab92553cc113
Andrew Rowson <github@growse.com>
2021-07-02 17:06:34 +0100

    Moved some generic espresso tests into the oss test suite

--- project/app/src/androidTestOss/java/org/owntracks/android/ui/AboutActivityTests.kt
+++ project/app/src/androidTestOss/java/org/owntracks/android/ui/AboutActivityTests.kt
@@ -33,8 +33,8 @@ class AboutActivityTests {
 
     @get:Rule
     val ruleChain: RuleChain = RuleChain
-            .outerRule(baristaRule.activityTestRule)
-            .around(screenshotRule)
+        .outerRule(baristaRule.activityTestRule)
+        .around(screenshotRule)
 
     @Before
     fun setUp() {
@@ -55,30 +55,50 @@ class AboutActivityTests {
     @AllowFlaky(attempts = 1)
     fun documentationLinkOpensSite() {
         clickOn(R.string.preferencesDocumentation)
-        intended(allOf(hasAction(Intent.ACTION_VIEW), hasData(baristaRule.activityTestRule.activity.getString(R.string.documentationUrl))))
+        intended(
+            allOf(
+                hasAction(Intent.ACTION_VIEW),
+                hasData(baristaRule.activityTestRule.activity.getString(R.string.documentationUrl))
+            )
+        )
     }
 
     @Test
     @AllowFlaky(attempts = 1)
     fun twitterLinkOpensSite() {
         clickOn(R.string.preferencesTwitter)
-        intended(allOf(hasAction(Intent.ACTION_VIEW), hasData(baristaRule.activityTestRule.activity.getString(R.string.twitterUrl))))
+        intended(
+            allOf(
+                hasAction(Intent.ACTION_VIEW),
+                hasData(baristaRule.activityTestRule.activity.getString(R.string.twitterUrl))
+            )
+        )
     }
 
     @Test
     @AllowFlaky(attempts = 1)
     fun sourceLinkOpensSite() {
         clickOn(R.string.preferencesRepository)
-        intended(allOf(hasAction(Intent.ACTION_VIEW), hasData(baristaRule.activityTestRule.activity.getString(R.string.repoUrl))))
+        intended(
+            allOf(
+                hasAction(Intent.ACTION_VIEW),
+                hasData(baristaRule.activityTestRule.activity.getString(R.string.repoUrl))
+            )
+        )
     }
 
     @Test
     @AllowFlaky(attempts = 1)
     fun translationLinkOpensSite() {
         clickOn(R.string.aboutTranslations)
-        intended(allOf(hasAction(Intent.ACTION_VIEW), hasData(baristaRule.activityTestRule.activity.getString(R.string.translationContributionUrl))))
+        intended(
+            allOf(
+                hasAction(Intent.ACTION_VIEW),
+                hasData(baristaRule.activityTestRule.activity.getString(R.string.translationContributionUrl))
+            )
+        )
     }
-    
+
     @Test
     @AllowFlaky(attempts = 1)
     fun librariesLinkListsLibraries() {
